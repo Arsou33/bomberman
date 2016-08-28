68195320707dbce23cfa2455bd4bb5aa002dc6c8
Arsouille <arsouille@peekmoon.org>
2016-08-28 15:20:32 +0200

    Display a simple triangle

---
+++ src/main/java/org/peekmoon/bomberman/Main.java
@@ -0,0 +1,116 @@
+package org.peekmoon.bomberman;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+
+
+import static org.lwjgl.system.MemoryUtil.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.lwjgl.glfw.Callbacks;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.opengl.GL;
+
+public class Main {
+    
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Bomberman starting...");
+		new Main().start();
+		System.out.println("Bomberman finished...");
+		
+	}
+
+    private void start() throws IOException, URISyntaxException {
+        GLFWErrorCallback errorCallback = GLFWErrorCallback.createPrint(System.err);
+        glfwSetErrorCallback(errorCallback);
+        
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+        
+        
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        
+        
+        long window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+        if (window == NULL) {
+            glfwTerminate();
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+        
+        glfwSetKeyCallback(window, keyCallback);
+        glfwMakeContextCurrent(window);
+        GL.createCapabilities();
+        
+
+        float points[] = {
+                0.0f,  0.5f,  0.0f,
+                0.5f, -0.5f,  0.0f,
+               -0.5f, -0.5f,  0.0f
+        };
+        
+        int vbo = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, points, GL_STATIC_DRAW);
+        
+        int vao = glGenVertexArrays();
+        glBindVertexArray(vao);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vao);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, NULL);
+        
+        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, new String(Files.readAllBytes(Paths.get(getClass().getResource("/shader.vertex").toURI()))));
+        glCompileShader(vertexShader);
+        
+        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, new String(Files.readAllBytes(Paths.get(getClass().getResource("/shader.fragment").toURI()))));
+        glCompileShader(fragmentShader);
+        
+        
+        int programShader = glCreateProgram();
+        glAttachShader(programShader, vertexShader);
+        glAttachShader(programShader, fragmentShader);
+        glLinkProgram(programShader);
+        
+        
+        
+        while (!glfwWindowShouldClose(window)) {
+            double time = glfwGetTime();
+            glClearColor(0.5f, 0.5f, 0.6f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glUseProgram(programShader);
+            glBindVertexArray(vao);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glfwPollEvents();
+            glfwSwapBuffers(window);
+        }
+        
+        glfwDestroyWindow(window);
+        Callbacks.glfwFreeCallbacks(window);
+        glfwTerminate();
+        errorCallback.free();
+    }
+    
+    private GLFWKeyCallback keyCallback = new GLFWKeyCallback() {
+        @Override
+        public void invoke(long window, int key, int scancode, int action, int mods) {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+                glfwSetWindowShouldClose(window, true);
+            }
+        }
+    };
+
+}
