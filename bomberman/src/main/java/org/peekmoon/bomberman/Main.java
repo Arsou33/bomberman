d49ecc9126d21ab90cad3c89fbc236064b0b5fa0
Arsouille <arsouille@peekmoon.org>
2016-10-22 00:29:44 +0200

    Improve : Display FPS (first part)

--- bomberman/src/main/java/org/peekmoon/bomberman/Main.java
+++ bomberman/src/main/java/org/peekmoon/bomberman/Main.java
@@ -1,36 +1,7 @@
 package org.peekmoon.bomberman;
 
-import static org.lwjgl.glfw.GLFW.GLFW_CONTEXT_VERSION_MAJOR;
-import static org.lwjgl.glfw.GLFW.GLFW_CONTEXT_VERSION_MINOR;
-import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_CORE_PROFILE;
-import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_FORWARD_COMPAT;
-import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_PROFILE;
-import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
-import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
-import static org.lwjgl.glfw.GLFW.glfwGetTime;
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
-import static org.lwjgl.glfw.GLFW.glfwPollEvents;
-import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
-import static org.lwjgl.glfw.GLFW.glfwSetFramebufferSizeCallback;
-import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowAspectRatio;
-import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
-import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
-import static org.lwjgl.glfw.GLFW.glfwTerminate;
-import static org.lwjgl.glfw.GLFW.glfwWindowHint;
-import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
-import static org.lwjgl.opengl.GL11.GL_LESS;
-import static org.lwjgl.opengl.GL11.GL_TRUE;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glClearColor;
-import static org.lwjgl.opengl.GL11.glDepthFunc;
-import static org.lwjgl.opengl.GL11.glDisable;
-import static org.lwjgl.opengl.GL11.glEnable;
-import static org.lwjgl.opengl.GL11.glViewport;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 import java.io.IOException;
@@ -55,7 +26,7 @@ public class Main {
 
     private final static Logger log = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) throws Exception {
         log.info("Bomberman starting...");
         new Main().start();
         log.info("Bomberman finished...");
@@ -71,7 +42,6 @@ public class Main {
 
     private void start() throws IOException, URISyntaxException {
         initOpenGlWindow();
-
 
         ProgramShader shader = new ProgramShader(new VertexShader("shader"), new FragmentShader("shader"));
         ProgramShader textShader = new ProgramShader(new VertexShader("text"), new FragmentShader("text"));
@@ -92,21 +62,9 @@ public class Main {
         KeyManager keyManager = new KeyManager(window, commandSender, camera);
         glfwSetKeyCallback(window, keyManager);
 
-        double lastTime = glfwGetTime();
-        double statEvery = 5;
-        double timeToStat = statEvery;
-        int nbFrame = 0;
+
         while (!glfwWindowShouldClose(window)) {
-            double time = glfwGetTime();
-            float elapsed = (float) (time - lastTime);
-            lastTime = time;
-            timeToStat -= elapsed;
-            nbFrame++;
-            if (timeToStat < 0) {
-                log.debug("FPS : {}", nbFrame / statEvery);
-                nbFrame = 0;
-                timeToStat = statEvery;
-            }
+
             GLUtils.checkError();
 
             glClearColor(0.5f, 0.5f, 0.6f, 1.0f);
@@ -117,12 +75,12 @@ public class Main {
             camera.update();
             GLUtils.checkError();
 
-            keyManager.update(elapsed);
+            keyManager.update(100); // TODO : Remove elpased ??
             GLUtils.checkError();
-            
+
             glEnable(GL_DEPTH_TEST);
             gameRenderer.render(statusReceiver.getStatus());
-            
+
             glDisable(GL_DEPTH_TEST);
             textShader.use();
             fpsRenderer.render();
@@ -136,11 +94,9 @@ public class Main {
     }
 
     private void initOpenGlWindow() {
-        errorCallback = GLFWErrorCallback.create(
-                (error, description) -> log.error("LWJGL Error - Code: {}, Description: {}",
-                Integer.toHexString(error), 
-                GLFWErrorCallback.getDescription(description))
-        );
+        errorCallback = GLFWErrorCallback
+                .create((error, description) -> log.error("LWJGL Error - Code: {}, Description: {}",
+                        Integer.toHexString(error), GLFWErrorCallback.getDescription(description)));
         glfwSetErrorCallback(errorCallback);
 
         if (!glfwInit()) {
--- bomberman/src/main/java/org/peekmoon/bomberman/opengl/GLTexture.java
+++ bomberman/src/main/java/org/peekmoon/bomberman/opengl/GLTexture.java
@@ -1,16 +1,6 @@
 package org.peekmoon.bomberman.opengl;
 
-import static org.lwjgl.opengl.GL11.GL_LINEAR;
-import static org.lwjgl.opengl.GL11.GL_RGBA;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_MAG_FILTER;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_MIN_FILTER;
-import static org.lwjgl.opengl.GL11.GL_UNSIGNED_BYTE;
-import static org.lwjgl.opengl.GL11.glBindTexture;
-import static org.lwjgl.opengl.GL11.glDeleteTextures;
-import static org.lwjgl.opengl.GL11.glGenTextures;
-import static org.lwjgl.opengl.GL11.glTexImage2D;
-import static org.lwjgl.opengl.GL11.glTexParameteri;
+import static org.lwjgl.opengl.GL11.*;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,23 +14,38 @@ public class GLTexture {
     private int id;
     
     public GLTexture(String texture) {
-        try (InputStream in = getClass().getResourceAsStream("/texture/" + texture)) {
-               PNGDecoder decoder = new PNGDecoder(in);
-               ByteBuffer buf = ByteBuffer.allocateDirect(4*decoder.getWidth()*decoder.getHeight());
-               decoder.decode(buf, decoder.getWidth()*4, Format.RGBA);
-               buf.flip();
-               id = glGenTextures();
-               glBindTexture(GL_TEXTURE_2D, id);
-               glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-               glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-               GLUtils.checkError("Unable parameter texture");
-               glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, decoder.getWidth(), decoder.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buf);
-               GLUtils.checkError("Unable transfer texture to GPU");
+        try (InputStream is = getClass().getResourceAsStream("/texture/" + texture)) {
+            init(is);
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
+
     }
     
+    public GLTexture(InputStream is) {
+        try {
+            init(is);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+    
+    private void init(InputStream is) throws IOException {
+        PNGDecoder decoder = new PNGDecoder(is);
+        ByteBuffer buf = ByteBuffer.allocateDirect(4 * decoder.getWidth() * decoder.getHeight());
+        decoder.decode(buf, decoder.getWidth() * 4, Format.RGBA);
+        buf.flip();
+        id = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, id);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        GLUtils.checkError("Unable parameter texture");
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, decoder.getWidth(), decoder.getHeight(), 0, GL_RGBA,
+                GL_UNSIGNED_BYTE, buf);
+        GLUtils.checkError("Unable transfer texture to GPU");
+    }
+
     public void release() {
         glDeleteTextures(id);
     }
